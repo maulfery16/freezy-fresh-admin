22339753fa69cc872a8c3d4350926829c2d5d7eb
Kiki <kiki.pratiwi.tif17@polban.ac.id>
2021-02-11 02:03:30 +0700

    feat(colour): adjust color service endpoint

--- src/services/colour.js
+++ src/services/colour.js
@@ -4,14 +4,15 @@ export default class ColourService extends RequestAdapterService {
 	async createColour(colour) {
 		try {
 			const { data } = await super.sendPostRequest(
-				`${this.baseUrl}/colour`,
+				`${this.baseUrl}/v1/colors`,
 				colour
 			);
 
 			return data;
 		} catch (error) {
+			console.error(error);
 			throw new Error(
-				`Fail creating colour: ${error.response.data.message}`
+				`Fail creating colour: ${error.response.data.message} - ${error.response.status} `
 			);
 		}
 	}
@@ -19,12 +20,13 @@ export default class ColourService extends RequestAdapterService {
 	async deleteColour(id) {
 		try {
 			return await super.sendDeleteRequest(
-				`${this.baseUrl}/colors/${id}`,
+				`${this.baseUrl}/v1/colors/${id}`,
 				{ id }
 			);
 		} catch (error) {
+			console.error(error);
 			throw new Error(
-				`Fail deleting admin: ${error.response.data.message}`
+				`Fail deleting colour: ${error.response.data.message} - ${error.response.status} `
 			);
 		}
 	}
@@ -32,14 +34,43 @@ export default class ColourService extends RequestAdapterService {
 	async editColour(id, colour) {
 		try {
 			const { data } = await super.sendPutRequest(
-				`${this.baseUrl}/colors/${id}`,
+				`${this.baseUrl}/v1/colors/${id}`,
 				colour
 			);
 
 			return data;
 		} catch (error) {
+			console.error(error);
 			throw new Error(
-				`Fail updating colour: ${error.response.data.message}`
+				`Fail updating colour: ${error.response.data.message} - ${error.response.status} `
+			);
+		}
+	}
+
+	async exportAsCSV(params, properties) {
+		try {
+			const { data } = await this.getColours(params);
+			super.dowloadDataAsCSV(data, properties, 'Colour List');
+		} catch (error) {
+			console.error(error);
+			throw new Error(
+				`Exporting Colour as CSV: ${error.response.data.message} - ${error.response.status} `
+			);
+		}
+	}
+
+	async getColours(params) {
+		try {
+			const { data } = await super.sendGetRequest(
+				`${this.baseUrl}/v1/colors`,
+				params
+			);
+
+			return data;
+		} catch (error) {
+			console.error(error);
+			throw new Error(
+				`Getting colour data: ${error.response.data.message} - ${error.response.status} `
 			);
 		}
 	}
@@ -47,13 +78,14 @@ export default class ColourService extends RequestAdapterService {
 	async getColourById(id) {
 		try {
 			const data = await super.sendGetRequest(
-				`${this.baseUrl}/colors/${id}`
+				`${this.baseUrl}/v1/colors/${id}`
 			);
 
 			return data;
 		} catch (error) {
+			console.error(error);
 			throw new Error(
-				`Fail getting colour detail: ${error.response.data.message}`
+				`Fail getting colour detail: ${error.response.data.message} - ${error.response.status} `
 			);
 		}
 	}
