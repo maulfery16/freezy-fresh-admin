97f0d878fba4a88373a3ac2b260253554c106e0f
Raka <mgf.prauliytama@gmail.com>
2021-02-10 21:53:04 +0700

    fix(category): various adjusments
    * change label on categoy modify page to bahasa
    * change request type when creating and editing category

--- src/services/category.js
+++ src/services/category.js
@@ -3,7 +3,7 @@ import RequestAdapterService from './request-adapter';
 export default class CategoryService extends RequestAdapterService {
 	async createCategory(category) {
 		try {
-			const { data } = await super.sendPostRequest(
+			const { data } = await super.sendPostMultipartRequest(
 				`${this.baseUrl}/v1/base_categories`,
 				category
 			);
@@ -41,7 +41,7 @@ export default class CategoryService extends RequestAdapterService {
 
 	async editCategory(id, category) {
 		try {
-			const { data } = await super.sendPutRequest(
+			const { data } = await super.sendPutMultipartRequest(
 				`${this.baseUrl}/v1/base_categories/${id}`,
 				category
 			);
