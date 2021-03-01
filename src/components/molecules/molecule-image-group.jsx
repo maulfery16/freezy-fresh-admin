4857da6faad147a8559eef92c8da48cda61b42bc
Raka <mgf.prauliytama@gmail.com>
2021-03-01 21:15:48 +0700

    feat(article): adjusting article related features
    * adjust article list page
    * adjust article add page
    * adjust article edit page
    * adjust article detail page
    * add react player library
    * adjust article service
    * add min-height of atom text editor

---
+++ src/components/molecules/molecule-image-group.jsx
@@ -0,0 +1,39 @@
+import { Col, Row, Space } from 'antd';
+import PropTypes from 'prop-types';
+import React from 'react';
+import AtomImage from '../atoms/image';
+
+const MoleculeImageGroup = (props) => {
+	return (
+		<Row
+			className="ba b--black-20 br3 pv5"
+			gutter={48}
+			justify="center"
+			style={{
+				marginLeft: '0px',
+				maxWidth: '100%',
+			}}
+		>
+			{props.images.map((image, index) => (
+				<Col key={`image_group_${index}`}>
+					<Space align="center" direction="vertical">
+						<AtomImage src={image.source} />
+
+						<p>{image.label}</p>
+					</Space>
+				</Col>
+			))}
+		</Row>
+	);
+};
+
+MoleculeImageGroup.propTypes = {
+	images: PropTypes.arrayOf(
+		PropTypes.shape({
+			label: PropTypes.string,
+			source: PropTypes.string,
+		})
+	),
+};
+
+export default MoleculeImageGroup;
