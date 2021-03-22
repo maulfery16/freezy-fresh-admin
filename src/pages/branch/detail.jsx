9227162c58bba82caed7d850999bc5da96c97e52
Kiki <kiki.pratiwi.tif17@polban.ac.id>
2021-03-22 22:46:18 +0700

    feat(branch): initialize branch pages * configure branch routes * construct branch list page * construct branch services

---
+++ src/pages/branch/detail.jsx
@@ -0,0 +1,175 @@
+import React, { useEffect, useState } from 'react';
+import ReactMoment from 'react-moment';
+
+import { Col, message, Row, Skeleton, Space, Typography } from 'antd';
+import { Link, useParams } from 'react-router-dom';
+
+import AtomCard from '../../../components/atoms/card';
+import AtomSecondaryButton from '../../../components/atoms/button/secondary-button';
+import MoleculeImageGroup from '../../../components/molecules/molecule-image-group';
+import MoleculeInfoGroup from '../../../components/molecules/info-group';
+import OrganismLayout from '../../../components/organisms/layout';
+
+import BannerService from '../../../services/banner';
+const bannerService = new BannerService();
+
+const BannerModifyPage = () => {
+	const { id } = useParams();
+	const [banner, setBanner] = useState(null);
+
+	const getBannerDetail = async () => {
+		try {
+			const { data: banner } = await bannerService.getBannerById(id);
+			setBanner(banner);
+		} catch (error) {
+			message.error(error.message);
+			console.error(error);
+		}
+	};
+
+	useEffect(() => {
+		(async () => {
+			getBannerDetail(id);
+		})();
+	}, []);
+
+	return (
+		<OrganismLayout
+			breadcumbs={[
+				{ name: 'Tampilan', link: '/view/banner' },
+				{ name: 'Banner', link: '/view/banner' },
+				{ name: 'Detail', link: location.pathname },
+			]}
+			title="Detail Banner"
+		>
+			<Typography.Title level={4}>
+				<span className="fw7">{`Detail Banner`.toUpperCase()}</span>
+			</Typography.Title>
+
+			{!banner ? (
+				<Skeleton active />
+			) : (
+				<Row align="top" className="mt4" gutter={24}>
+					<Col span={15}>
+						<AtomCard title="Info Banner">
+							<Row gutter={[12, 24]}>
+								<Col span={24}>
+									<MoleculeInfoGroup
+										title="Foto Banner"
+										content={
+											<MoleculeImageGroup
+												images={[
+													{
+														source:
+															banner.image_mobile,
+														label:
+															' Foto Banner Mobile',
+													},
+													{
+														source:
+															banner.image_dekstop,
+														label:
+															' Foto Banner Dekstop',
+													},
+												]}
+											/>
+										}
+									/>
+								</Col>
+
+								<Col span={12}>
+									<MoleculeInfoGroup
+										title="Title Banner (ID)"
+										content={banner.title.id}
+									/>
+								</Col>
+
+								<Col span={12}>
+									<MoleculeInfoGroup
+										title="Title Banner (EN)"
+										content={banner.title.en}
+									/>
+								</Col>
+
+								<Col span={12}>
+									<MoleculeInfoGroup
+										title="Cabang"
+										content={banner.branches
+											.map((branch) => branch.name)
+											.join(', ')}
+									/>
+								</Col>
+
+								<Col span={12}>
+									<MoleculeInfoGroup
+										title="Nama Promo (ID)"
+										content={
+											banner.promotion &&
+											banner.promotion.name
+										}
+									/>
+								</Col>
+
+								<Col span={24} style={{ paddingTop: '2rem' }}>
+									<Typography.Text strong>
+										<span className="denim f5">
+											INFO UPDATE PRODUK
+										</span>
+									</Typography.Text>
+								</Col>
+
+								<Col span={12}>
+									<MoleculeInfoGroup
+										title="Tanggal di Daftarkan"
+										content={
+											<ReactMoment format="DD-MM-YYYY">
+												{banner.created_at}
+											</ReactMoment>
+										}
+									/>
+								</Col>
+
+								<Col span={12}>
+									<MoleculeInfoGroup
+										title="Tanggal di Update"
+										content={
+											<ReactMoment format="DD-MM-YYYY">
+												{banner.updated_at}
+											</ReactMoment>
+										}
+									/>
+								</Col>
+
+								<Col span={12}>
+									<MoleculeInfoGroup
+										title="Didaftarkan Oleh"
+										content={banner.created_by}
+									/>
+								</Col>
+
+								<Col span={12}>
+									<MoleculeInfoGroup
+										title="Diupdate Oleh"
+										content={banner.updated_by}
+									/>
+								</Col>
+							</Row>
+						</AtomCard>
+					</Col>
+
+					<Col className="mt4" span={24}>
+						<Space>
+							<Link to="/view/banner">
+								<AtomSecondaryButton size="large">
+									Kembali
+								</AtomSecondaryButton>
+							</Link>
+						</Space>
+					</Col>
+				</Row>
+			)}
+		</OrganismLayout>
+	);
+};
+
+export default BannerModifyPage;
---
+++ src/pages/branch/index.jsx
@@ -0,0 +1,180 @@
+/* eslint-disable react/display-name */
+import React, { useRef } from 'react';
+import { Link } from 'react-router-dom';
+import { Space } from 'antd';
+import { EditFilled, EyeFilled } from '@ant-design/icons';
+
+import AtomStatusSwitch from '../../components/atoms/datatable/status-switch';
+import MoleculeDatatableAdditionalAction from '../../components/molecules/datatable/additional-actions';
+import MoleculeDatatableDateRange from '../../components/molecules/datatable/date-range-plugin';
+import MoleculeDatatableFilter from '../../components/molecules/datatable/filter-plugin';
+import MoleculeDeleteConfirm from '../../components/molecules/delete-confirm';
+import OrganismDatatable from '../../components/organisms/datatable';
+import OrganismLayout from '../../components/organisms/layout';
+
+const BranchPage = () => {
+	const column = [
+		{
+			align: 'center',
+			title: 'No',
+			dataIndex: 'id',
+			render: (id, _, index) => index + 1,
+		},
+		{
+			title: 'Kode Cabang',
+			dataIndex: 'code',
+			sorter: true,
+		},
+		{
+			title: 'Nama Cabang (ID)',
+			dataIndex: 'name[id]',
+			render: (_, record) => record.name.id,
+			sorter: true,
+		},
+		{
+			title: 'Nama Cabang (EN)',
+			dataIndex: 'name[en]',
+			render: (_, record) => record.name.en,
+			sorter: true,
+		},
+		{
+			title: 'Provinsi',
+			dataIndex: 'address[province_name]',
+			render: (_, record) => record.address.province_name,
+			sorter: true,
+		},
+		{
+			title: 'Kota/Kabupaten',
+			dataIndex: 'address[city_name]',
+			render: (_, record) => record.address.city_name,
+			sorter: true,
+		},
+		{
+			title: 'Kecamatan',
+			dataIndex: 'address[district_name]',
+			render: (_, record) => record.address.district_name,
+			sorter: true,
+		},
+		{
+			title: 'Kelurahan',
+			dataIndex: 'address[subdistrict_name]',
+			render: (_, record) => record.address.subdistrict_name,
+			sorter: true,
+		},
+		{
+			title: 'Alamat',
+			dataIndex: 'address[address]',
+			render: (_, record) => record.address.address,
+			sorter: true,
+		},
+		{
+			title: 'Kode Pos',
+			dataIndex: 'address[postal_code]',
+			render: (_, record) => record.address.postal_code,
+			sorter: true,
+		},
+		{
+			align: 'center',
+			title: 'Aktif',
+			dataIndex: 'is_active',
+			render: (active, record) => (
+				<AtomStatusSwitch
+					active={active}
+					id={record.id}
+					tableRef={branchTableRef}
+					url="branches"
+				/>
+			),
+			csvRender: (item) => (item.is_active ? 'Aktif' : 'Tidak Aktif'),
+		},
+		{
+			align: 'center',
+			title: 'Aksi',
+			dataIndex: 'id',
+			render: (id, record) => (
+				<Space size="middle">
+					<Link to={`/branch/${id}/detail`}>
+						<EyeFilled className="f4 blue" />
+					</Link>
+
+					<Link to={`/branch/${id}/edit`}>
+						<EditFilled className="f4 orange" />
+					</Link>
+
+					{!record.is_active && (
+						<MoleculeDeleteConfirm
+							id={id}
+							label="Branch"
+							tableRef={branchTableRef}
+							url="branches"
+						/>
+					)}
+				</Space>
+			),
+			skipExport: true,
+		},
+	];
+	const branchTableRef = useRef();
+
+	const renderAdditionalAction = () => {
+		return (
+			<MoleculeDatatableAdditionalAction
+				column={column}
+				label="Branch"
+				getLimit={() => branchTableRef.current.totalData}
+				route="/branch"
+				url="branches"
+			/>
+		);
+	};
+
+	const renderDatatableFilters = () => {
+		return [
+			<MoleculeDatatableFilter
+				identifier="status-filter"
+				key="status"
+				label="Status"
+				name="is_active"
+				operator=":"
+				placeholder="Semua status"
+				data={{
+					mock: [
+						{
+							value: 'true',
+							label: 'Aktif',
+						},
+						{ value: 'false', label: 'Tidak Aktif' },
+					],
+				}}
+			/>,
+			<MoleculeDatatableDateRange
+				identifier="daterange-filter"
+				key="daterange"
+				label="Tanggal Pendaftaran"
+				name="created_at"
+				operator=":"
+				placeholder="Filter tanggal pendaftaran"
+			/>,
+		];
+	};
+
+	return (
+		<OrganismLayout
+			breadcumbs={[{ name: 'Branch', link: '/branch' }]}
+			title="Branch Page"
+		>
+			<OrganismDatatable
+				additionalAction={renderAdditionalAction()}
+				columns={column}
+				dataSourceURL={`branches`}
+				filters={renderDatatableFilters()}
+				ref={branchTableRef}
+				scroll={1920}
+				searchInput={true}
+				title={`Branch Menu`}
+			/>
+		</OrganismLayout>
+	);
+};
+
+export default BranchPage;
